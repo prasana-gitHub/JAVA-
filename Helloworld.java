a7160f1bebc65a6559744f1770e92e12534787bf
PRASANA PATTANAYAK <amit.pattanayak45@gmail.com>
2024-11-28 17:08:25 +0000

    Hello World Programm

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Welcome to the Java World");
+        System.out.println("This is my first java program");
+
+
+        
+        // This is a print without using a semi colon;
+        if(System.out.printf("Hellow Amit")==null){
+        }
+    }
+}
