a0d625a71e12c5d78bf3b9308b1af7535b08f2ff
PRASANA PATTANAYAK <amit.pattanayak45@gmail.com>
2024-11-29 13:56:36 +0000

    Operators

---
+++ Operators.java
@@ -0,0 +1,87 @@
+public class Operators{
+    public static void main(String[]args){
+        /*int num1 = 10;
+        int num2 = 20;
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+                                                    Addition program
+        int sum = num1+num2;
+        System.out.println("The sum = " + sum);*/
+
+
+        /*int num1 = 10;
+        int num2 = 20;
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+                                                Substraction Program
+        int sub = num1-num2;
+
+        System.out.println("The Sub is = " + sub);*/
+
+
+        /*int num1 = 10;
+        int num2 = 20;
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+                                                Multiplication Program
+        int mul = num1*num2;
+
+        System.out.println("The Mul is  = " + mul);*/
+
+
+        /*int num1 = 20;
+        int num2 = 10;
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        float div = num1/num2;
+        System.out.println("The div = " + div);*/
+
+        /*int num1 = 9;
+        int num2 = 2;
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        int mod = num1%num2;
+        System.out.println("The mod is = " + mod);*/
+
+
+        /*int x = 10;
+        System.out.println("x is " + x);
+
+        x++;
+        System.out.println("after change the value of x is = " + x);*/
+
+
+        /*int  y = 5;
+        System.out.println("now the value of y is = " + y);
+
+        --y;
+        System.out.println("The new value of y is  = " + y);*/
+
+
+        /*int var1 = 5 ;
+        int var2 = 10;
+        int var3 = 5 ; 
+        System.out.println("var1 = " + var1);
+        System.out.println("var2 = " + var2);
+        System.out.println("var3 = " + var3);
+                                                    Equal to operators
+        System.out.println("var1 == var2 : " + (var1 == var2));
+        System.out.println("var1 == var3 : " + (var1 == var3));*/ 
+
+
+
+        /*int var1 = 5;
+        int var2 = 10;
+        int var3 = 5 ; 
+        System.out.println("var1 = " + var1);
+        System.out.println("var2 = " + var2);
+        System.out.println("var3 = " + var3);
+                                                    Not equal to’ Operator(!=)
+        System.out.println("var1 != var2: " + (var1!=var2));
+        System.out.println("var1 != var3: " + (var1!=var3));*/
+
+
+    }
+}
